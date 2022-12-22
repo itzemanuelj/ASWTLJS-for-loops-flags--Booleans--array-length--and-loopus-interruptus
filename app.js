75fa2edb97c371db722d339dad540e6012f4dc1d
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-22 13:16:34 -0500

    mvp commit

---
+++ app.js
@@ -0,0 +1,61 @@
+///In one word, what is a flag?
+/// a variable
+
+///What is the keyword that stops a loop from continuing to loop?
+for (let i = 0; i<=10; i++) {
+    if (i === 3){
+break;
+    }
+}
+
+///There are two Booleans. Name either one of them.
+var a = true;
+var b = false;
+
+///The statement assigns the number of elements in the array to the variable. Fill in the blank.
+let num = generals.length;
+
+///Find the number of elements in the array cities and assign the number to the variable numCities, which hasn't been declared beforehand.
+
+let numCities = cities.length;
+
+///Code two lines. The first line reverses the flag x, which was originally set to the positive Boolean. The second line stops the looping.
+
+x = false;
+//break;
+
+let c = true;
+
+///Find the number of elements in an array and assign the number to a variable, which hasn't been declared beforehand.
+
+let numberLenght = array.lenght;
+
+///Code the first line of a for loop with the usual counter, the usual starting value, and the usual incrementing. Limit the number of loops by the number of elements in the array pets.
+
+for (let i = 0; i < pets.length; i++) {
+    if (pets.lenght === 3);
+{
+    break;
+}
+}
+
+///In 3 statements, assign a number to a variable that's already been declared, assign a Boolean (either one) to a flag that's already been declared, and interrupt a loop. Don't indent any of the lines.
+
+let randomVar  = 1;
+cost = true;
+// break;
+
+
+///Complete this code to display an alert if a match isn't found.
+let matchFound = false;
+for (let i = 0; i < list.length; i++) {
+  if (userInput === list[i]) {
+    alert("Match found");
+    matchFound = true;
+    break;
+    if (matchFound === false) {
+    }
+    alert("No match found");
+  }
+}
+
